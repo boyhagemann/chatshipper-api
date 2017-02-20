cd6613e6b2916fc20d4f0e413c105ee8c1a8b561
boyhagemann <boyhagemann@gmail.com>
2017-02-20 09:25:11 +0100

    fetch selection

--- app.js
+++ app.js
@@ -1,38 +1,59 @@
 var express = require('express')
 var app = express()
 var fetch = require('isomorphic-fetch')
-// var firebase = require('firebase');
+var firebase = require('firebase')
 var cors = require('cors')
+
+app.use(cors())
 
 
 // Initialize Firebase
-  // var config = {
-  //   apiKey: "AIzaSyCtlLjOM77psPuIhen0xXaAP9nWI1Io1lw",
-  //   authDomain: "chatshipper-demo.firebaseapp.com",
-  //   databaseURL: "https://chatshipper-demo.firebaseio.com",
-  //   storageBucket: "chatshipper-demo.appspot.com",
-  //   messagingSenderId: "928848885077"
-  // };
-  // firebase.initializeApp(config);
+ var config = {
+   apiKey: "AIzaSyCtlLjOM77psPuIhen0xXaAP9nWI1Io1lw",
+   authDomain: "chatshipper-demo.firebaseapp.com",
+   databaseURL: "https://chatshipper-demo.firebaseio.com",
+   storageBucket: "chatshipper-demo.appspot.com",
+   messagingSenderId: "928848885077"
+ };
+ firebase.initializeApp(config);
 
 
+ app.get('/selection', function (req, res) {
 
-app.use(cors())
+   firebase.database().ref('/places').once('value').then(function(snapshot) {
+     res.json(snapshot.val())
+   })
+ });
+
+app.get('/save/:id/:name', function (req, res) {
+
+  const id = req.params.id
+  const name = req.params.name
+
+  firebase.database().ref('/places/' + id).set({
+    id: id,
+    name: name
+  })
+
+  res.json({
+    status: "ok"
+  })
+});
 
 app.get('/search/:query', function (req, res) {
 
   const q = req.params.query;
 
   fetch('https://maps.googleapis.com/maps/api/place/textsearch/json?key=AIzaSyDLy2CJ6vd5Iy1fDGlRksltNukJLhCBtLg&query=' + q + '&language=nl', {
-    //  mode: 'cors'
+     mode: 'cors'
    })
      .then( function(response) {
 
-        console.log(response.status)
+        // console.log(response.status)
          if (response.status >= 400) {
              throw new Error("Bad response from server")
          }
-         console.log(response.json())
+        //  console.log(response.json())
          return response.json()
      })
      .then(
@@ -47,16 +68,6 @@ app.get('/search/:query', function (req, res) {
      )
 
 })
-
-
-// app.get('/save/:id/:name', function (req, res) {
-//
-//   firebase.database().ref('/').set({
-//       id: req.params.id,
-//       name: req.params.name
-//   });
-//
-// })
 
 app.listen(3001, function () {
   console.log('Example app listening on port 3001!')
