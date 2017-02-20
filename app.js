b02c0bc27571af8196dc8af0e4ec490a4c6d7ae3
boyhagemann <boyhagemann@gmail.com>
2017-02-20 15:51:26 +0100

    updated routes

--- app.js
+++ app.js
@@ -3,37 +3,36 @@ var app = express()
 var fetch = require('isomorphic-fetch')
 var firebase = require('firebase')
 var cors = require('cors')
+var bodyParser = require('body-parser')
 
 app.use(cors())
 
 
 // Initialize Firebase
- var config = {
-   apiKey: "AIzaSyCtlLjOM77psPuIhen0xXaAP9nWI1Io1lw",
-   authDomain: "chatshipper-demo.firebaseapp.com",
-   databaseURL: "https://chatshipper-demo.firebaseio.com",
-   storageBucket: "chatshipper-demo.appspot.com",
-   messagingSenderId: "928848885077"
- };
- firebase.initializeApp(config);
+var config = {
+ apiKey: "AIzaSyCtlLjOM77psPuIhen0xXaAP9nWI1Io1lw",
+ authDomain: "chatshipper-demo.firebaseapp.com",
+ databaseURL: "https://chatshipper-demo.firebaseio.com",
+ storageBucket: "chatshipper-demo.appspot.com",
+ messagingSenderId: "928848885077"
+};
+firebase.initializeApp(config);
 
 
- app.get('/selection', function (req, res) {
+app.get('/selection', function (req, res) {
 
-   firebase.database().ref('/places').once('value').then(function(snapshot) {
+   firebase.database().ref('/places')
+    .orderByChild('name')
+    .once('value')
+    .then(function(snapshot) {
      res.json(snapshot.val())
    })
  });
 
-app.get('/save/:id/:name', function (req, res) {
+app.post('/save', bodyParser.json(), function (req, res) {
 
-  const id = req.params.id
-  const name = req.params.name
-
-  firebase.database().ref('/places/' + id).set({
-    id: id,
-    name: name
-  })
+  console.log(req.body)
+  firebase.database().ref('/places/' + req.body.id).set(req.body)
 
   res.json({
     status: "ok"
@@ -44,25 +43,18 @@ app.get('/search/:query', function (req, res) {
 
   const q = req.params.query;
 
-  fetch('https://maps.googleapis.com/maps/api/place/textsearch/json?key=AIzaSyDLy2CJ6vd5Iy1fDGlRksltNukJLhCBtLg&query=' + q + '&language=nl', {
-     mode: 'cors'
-   })
+  fetch('https://maps.googleapis.com/maps/api/place/textsearch/json?key=AIzaSyDLy2CJ6vd5Iy1fDGlRksltNukJLhCBtLg&query=' + q + '&language=nl')
      .then( function(response) {
-
-        // console.log(response.status)
          if (response.status >= 400) {
              throw new Error("Bad response from server")
          }
-        //  console.log(response.json())
          return response.json()
      })
      .then(
        function(response) {
-         console.log(response)
-         res.json(response);
+         res.json(response)
        },
        function(error) {
-         console.log(error)
          res.json(error)
        }
      )
